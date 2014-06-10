1c91018d3c2e0b3fac4af75f7b44ef842d22a2c8
Gar <gar@yeah.net>
2014-06-10 11:47:12 +0800

    no chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
-  #get "static_pages/home"
-  #match '/', to:'static_pages#home', via:'get'
-  match '/help', to:'static_pages#help', via:'get'
+SampleApp::Application.routes.draw do
+
+  get "users/new"
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
